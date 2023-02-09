4246692405b0404ba49c755a1f9b8e185f3ee5f0
yeargun <yeargun24@gmail.com>
2023-02-09 22:43:38 +0300

    post classrefact

--- auth/src/main/java/com/yeargun/auth/postrecommendation/Post.java
+++ auth/src/main/java/com/yeargun/auth/postrecommendation/Post.java
@@ -3,7 +3,6 @@ package com.yeargun.auth.postrecommendation;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
-import java.util.HashMap;
 import java.util.List;
 
 public class Post {
